87622beec4c1f6d270ab7466616e6f6507b3899c
Bakhrom Usmanov <bakhromusmanov01@gmail.com>
2024-05-09 18:02:37 +0500

    Spawn and Respawn the Bird

--- Assets/Scripts/Bird.cs
+++ Assets/Scripts/Bird.cs
@@ -5,23 +5,27 @@ using UnityEngine.InputSystem;
 
 public class Bird : MonoBehaviour
 {
+    [SerializeField] GameObject bird1Prefab;
+    [SerializeField] GameObject bird2Prefab;
+    [SerializeField] Rigidbody2D pivot;
+    [SerializeField] float detachDelay;
+    [SerializeField] float respawnDelay;
 
     Camera mainCamera;
     Rigidbody2D currentRigidBody;
     Animator birdAnimator;
+    SpringJoint2D currentSpring;
+    bool isDragging;
+    GameObject[] Birds = new GameObject[2];
+    
 
-    [SerializeField] GameObject birdPrefab;
-    public bool isDragging;
 
-    private void Awake() 
-    {
-        
-    }
-    void Start()
+    void Awake()
     {
         mainCamera = Camera.main;
-        currentRigidBody = birdPrefab.GetComponent<Rigidbody2D>();
-        birdAnimator = birdPrefab.GetComponent<Animator>();
+        Birds[0] =bird1Prefab;
+        Birds[1] =bird2Prefab;
+        SpawnBird();
     }
 
     // Update is called once per frame
@@ -49,6 +53,29 @@ public class Bird : MonoBehaviour
     void LaunchBird()
     {
         currentRigidBody = null;
+        Invoke(nameof(DetachSpring), detachDelay);
+    }
+
+    void DetachSpring ()
+    {
+        birdAnimator.SetBool("isLaunched", true);
+        currentSpring.enabled = false;
+        currentSpring = null;
+
+        Invoke(nameof(SpawnBird), respawnDelay);
+    }
+    
+    void SpawnBird()
+    {
+        GameObject birdInstance = Instantiate(Birds[Random.Range(0,2)], pivot.transform.position-new Vector3(2,2,0), Quaternion.identity, GameObject.Find("Birds").transform);
+        currentRigidBody = birdInstance.GetComponent<Rigidbody2D>();
+        birdAnimator = birdInstance.GetComponent<Animator>();
+
+        currentSpring = birdInstance.GetComponent<SpringJoint2D>();
+        currentSpring.enabled = true;
+        currentSpring.connectedBody=pivot;
+
+        birdAnimator = birdInstance.GetComponent<Animator>();
     }
 
 }
--- Assets/Scripts/SpringDetach.cs
+++ Assets/Scripts/SpringDetach.cs
@@ -4,31 +4,6 @@ using UnityEngine;
 
 public class SpringDetach : MonoBehaviour
 {
-    SpringJoint2D currentSpring;
-    Animator animator;
-    Bird bird;
-    private void Awake() {
-        
-    }
-    private void Start() {
-        currentSpring = GetComponent<SpringJoint2D>();
-        animator = GetComponent<Animator>();
-        bird = FindObjectOfType<Bird>();
-    }
-     private void OnTriggerEnter2D(Collider2D other) 
-    {
-        if(other.tag == "Catapult")
-        {
-            if(currentSpring!=null && !bird.isDragging)
-            {
-                animator.SetBool("isLaunched", true);
-                DetachSpring ();
-            }
-        }
-    }
-    void DetachSpring ()
-    {
-        currentSpring.enabled = false;
-        currentSpring = null;
-    }
+    
+    
 }
